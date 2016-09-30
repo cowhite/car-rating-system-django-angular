66a646959975974d3b82972bf558ff4d0851dd88
ganesh <ganesh@cowhite.com>
2016-09-30 15:18:24 +0530

    urls updated

--- project_template/reviews/urls.py
+++ project_template/reviews/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^cars/models/$', CarModelsView.as_view()),
-    url(r'^cars/models/(?P<pk>\d+)/?$', CarModelView.as_view()),
-    url(r'^cars/brand/$', CarBrandView.as_view(), name="car_brand"),
-    url(r'^cars/variant/$', CarsVariantView.as_view()),
-    url(r'^cars/review/$', CarVariantReviewView.as_view()),
+    url(r'^api/cars/models/$', CarModelsView.as_view()),
+    url(r'^api/cars/models/(?P<pk>\d+)/?$', CarModelView.as_view()),
+    url(r'^api/cars/brand/$', CarBrandView.as_view(), name="car_brand"),
+    url(r'^api/cars/variant/$', CarsVariantView.as_view()),
+    url(r'^api/cars/review/$', CarVariantReviewView.as_view()),
 
 ]
